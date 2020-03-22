d6f7fa7c5ad6b935b5528bb021498d73a5b4793f
Adler <24289271+IridescentL@users.noreply.github.com>
2020-03-22 23:01:35 +0800

    fix: add template and remove old files

--- src/main/java/pretty/april/achieveitserver/AchieveitServerApplication.java
+++ src/main/java/pretty/april/achieveitserver/AchieveitServerApplication.java
@@ -2,8 +2,10 @@ package pretty.april.achieveitserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement(proxyTargetClass = true)
 public class AchieveitServerApplication {
 
     public static void main(String[] args) {
