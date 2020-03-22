cadb6dfb3099bc23ee790e7503c04c32d020e1fd
Adler <24289271+IridescentL@users.noreply.github.com>
2020-03-22 22:09:02 +0800

    fix confilcts

--- src/main/java/pretty/april/achieveitserver/entity/Project.java
+++ src/main/java/pretty/april/achieveitserver/entity/Project.java
@@ -5,7 +5,6 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
-git
 import java.io.Serializable;
 import java.time.LocalDate;
 
