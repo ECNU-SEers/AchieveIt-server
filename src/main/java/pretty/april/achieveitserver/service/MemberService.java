f740fb6af0356a27be4fc87fd40910a17a0c8d5c
Adler <24289271+IridescentL@users.noreply.github.com>
2020-03-23 12:27:16 +0800

    fix: remove useless dependencies

--- src/main/java/pretty/april/achieveitserver/service/MemberService.java
+++ src/main/java/pretty/april/achieveitserver/service/MemberService.java
@@ -1,6 +1,8 @@
 package pretty.april.achieveitserver.service;
 
 import org.springframework.stereotype.Service;
+import pretty.april.achieveitserver.entity.Project;
+import pretty.april.achieveitserver.entity.ProjectMember;
 import pretty.april.achieveitserver.mapper.ProjectMapper;
 import pretty.april.achieveitserver.mapper.ProjectMemberMapper;
 import pretty.april.achieveitserver.request.AddProjectMemberRequest;
@@ -13,9 +15,17 @@ public class MemberService {
     private ProjectMapper projectMapper;
 
     public void addProjectMember(AddProjectMemberRequest request, Integer projectId) {
-        if (projectMapper.selectById(projectId) == null) {
+        Project project = projectMapper.selectById(projectId);
+        if (project == null) {
             throw new IllegalArgumentException("Cannot find project");
         }
+        ProjectMember projectMember = new ProjectMember();
+        projectMember.setProjectId(projectId);
+        projectMember.setProjectName(project.getName());
+        projectMember.setUserId(request.getUserId());
+        projectMember.setUsername(request.getUsername());
+        projectMember.setLeaderId(request.getLeaderId());
+        projectMember.setLeaderName(request.getLeaderName());
 
     }
 }
