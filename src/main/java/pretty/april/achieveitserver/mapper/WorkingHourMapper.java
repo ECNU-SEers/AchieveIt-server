553d0fc34a06cef11a8c39b128ad9f0bfd877e7a
Jialei_Chen <Jialei_Chen9854@outlook.com>
2020-04-03 21:30:59 +0800

    add @Param

--- src/main/java/pretty/april/achieveitserver/mapper/WorkingHourMapper.java
+++ src/main/java/pretty/april/achieveitserver/mapper/WorkingHourMapper.java
@@ -2,6 +2,7 @@ package pretty.april.achieveitserver.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import pretty.april.achieveitserver.entity.LaborHour;
 import pretty.april.achieveitserver.model.WorkingHour;
@@ -12,5 +13,5 @@ import java.util.List;
 public interface WorkingHourMapper extends BaseMapper<LaborHour> {
 
     @Select("SELECT COALESCE(SUM(TIME_TO_SEC(end_time)-TIME_TO_SEC(start_time)),0) FROM labor_hour WHERE state = '已通过' AND user_id = #{userId} AND project_id = #{projectId}")
-    Float selectWorkingHour(Integer userId, Integer projectId);
+    Float selectWorkingHour(@Param("userId")Integer userId, @Param("projectId")Integer projectId);
 }
