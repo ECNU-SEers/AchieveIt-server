196aa7d93b3ceb9b74999bb31fde6e6ea7df7c9d
Jialei_Chen <Jialei_Chen9854@outlook.com>
2020-04-18 11:13:35 +0800

    add device permission members api

--- src/test/java/pretty/april/achieveitserver/service/ProjectMemberServiceTest.java
+++ src/test/java/pretty/april/achieveitserver/service/ProjectMemberServiceTest.java
@@ -19,40 +19,40 @@ public class ProjectMemberServiceTest {
 
 	@Autowired
 	private ProjectMemberService projectMemberService;
-//	
-//	@Test
-//	public void selectCountByProjectIdTest() {
-//		Integer projectId = new Integer(57);
-//		assertEquals(0, projectMemberService.selectCountByProjectId(projectId));
-//	}
-//	
-//	@Test
-//	public void selectByProjectIdTest() {
-//		Integer projectId = new Integer(1);
-//		List<ProjectMember> projectMembers = projectMemberService.selectByProjectId(projectId);
-//		assertEquals(1, projectMembers.size());
-//	}
-//	
-//	@Test
-//	public void checkMemberExistByProjectIdAndUserIdTest_userId_1() {
-//		Integer projectId = new Integer(1);
-//		Integer userId = new Integer(1);
-//		assertTrue(projectMemberService.checkMemberExistByProjectIdAndUserId(projectId, userId));
-//	}
-//	
-//	@Test
-//	public void checkMemberExistByProjectIdAndUserIdTest_userId_2() {
-//		Integer projectId = new Integer(1);
-//		Integer userId = new Integer(2);
-//		assertFalse(projectMemberService.checkMemberExistByProjectIdAndUserId(projectId, userId));
-//	}
-//	
-//	@Test
-//	public void selectProjectIdByUserIdTest() {
-//		Integer userId = new Integer(1);
-//		assertNotNull(projectMemberService.selectProjectIdByUserId(userId).size());
-//	}
-//	
+	
+	@Test
+	public void selectCountByProjectIdTest() {
+		Integer projectId = new Integer(57);
+		assertEquals(0, projectMemberService.selectCountByProjectId(projectId));
+	}
+	
+	@Test
+	public void selectByProjectIdTest() {
+		Integer projectId = new Integer(1);
+		List<ProjectMember> projectMembers = projectMemberService.selectByProjectId(projectId);
+		assertEquals(1, projectMembers.size());
+	}
+	
+	@Test
+	public void checkMemberExistByProjectIdAndUserIdTest_userId_1() {
+		Integer projectId = new Integer(1);
+		Integer userId = new Integer(1);
+		assertTrue(projectMemberService.checkMemberExistByProjectIdAndUserId(projectId, userId));
+	}
+	
+	@Test
+	public void checkMemberExistByProjectIdAndUserIdTest_userId_2() {
+		Integer projectId = new Integer(1);
+		Integer userId = new Integer(2);
+		assertFalse(projectMemberService.checkMemberExistByProjectIdAndUserId(projectId, userId));
+	}
+	
+	@Test
+	public void selectProjectIdByUserIdTest() {
+		Integer userId = new Integer(1);
+		assertNotNull(projectMemberService.selectProjectIdByUserId(userId).size());
+	}
+	
 	
 	@Test
 	public void selectProjectMembersWithDeviceQueryAndManagementPermissionTest() {
